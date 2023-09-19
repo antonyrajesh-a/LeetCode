c5e913ac767a7902466d4034e65223896d9a4f85
A.Antony Rajesh <104267267+antonyrajesh-a@users.noreply.github.com>
2023-09-19 17:57:21 +0530

    Time: 4 ms (31.85%), Space: 9.3 MB (8.01%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -6,10 +6,10 @@ public:
         
         while (i >= 0 && j >= 0) {
             
-            if(nums1[i] > nums2[j]){
-                nums1[k--] = nums1[i--];
-            } else {
+            if(nums1[i] < nums2[j]){
                 nums1[k--] = nums2[j--];
+            } else {
+                nums1[k--] = nums1[i--];
             }
         }
         
