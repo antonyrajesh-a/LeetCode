312e80ba479f366d5bce11200779b3bf8f1ac167
A.Antony Rajesh <104267267+antonyrajesh-a@users.noreply.github.com>
2023-09-20 19:26:52 +0530

    Time: 21 ms (68.67%), Space: 26 MB (9.16%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         k = k % n;
         vector<int> temp;
-        if( n > 0){
+        if( n > 1){
             for ( int i = n-k; i <= n-1 ; i++) temp.push_back(nums[i]);
         
             for ( int i = n-1; i >= k ; --i ) nums[i] = nums[i-k];                                       
