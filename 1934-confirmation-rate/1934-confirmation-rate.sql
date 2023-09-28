8dbff12cfa9b9b03e5bc10b51aa319c0d2364d4c
A.Antony Rajesh <104267267+antonyrajesh-a@users.noreply.github.com>
2023-09-28 19:26:33 +0530

    Time: 1625 ms (41.95%), Space: 0B (100.00%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,8 +1,2 @@
-# Write your MySQL query statement below
-
-select a.id as user_id,ifnull(round(b.confo_count/a.total,2),0) as confirmation_rate from(
-select ifnull(temp.county,0) as total, ups.user_id as id from Signups as ups left join (select count(user_id) as county ,user_id from Confirmations   group by user_id) as temp on ups.user_id= temp.user_id
-) as a 
-join
-(select ifnull(temp.county,0) as confo_count, ups.user_id as id from Signups as ups left join (select count(user_id) as county ,user_id from Confirmations  where action ='confirmed' group by user_id) as temp on ups.user_id= temp.user_id )as b on a.id = b.id;
-
+select s.user_id, round(avg(if(c.action="confirmed",1,0)),2) as confirmation_rate
+from Signups as s left join Confirmations as c on s.user_id= c.user_id group by user_id;
